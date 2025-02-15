ac021d072520d73271aaf73f9dc35190cfbfb60b
Shawn Michael Larkin <shawn.michael.larkin@gmail.com>
2025-02-15 14:03:24 -0800

    style: add padding line between statements eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,15 @@
 module.exports = {
   extends: "expo",
   ignorePatterns: ["/dist/*"],
+  rules: {
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "*" },
+      { blankLine: "any", prev: "import", next: "import" },
+      { blankLine: "any", prev: "expression", next: "expression" },
+      { blankLine: "any", prev: "const", next: "const" },
+      { blankLine: "any", prev: "let", next: "let" },
+      { blankLine: "any", prev: "var", next: "var" }
+    ]
+  }
 };
