a132f0b3794bf8066ae1a2ead618ad39982bcd86
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-02-18 00:58:08 +0000

    feat: init drizzle and supabase (#5)
    * feat: init drizzle
    
    * setup supabase
    
    * fix: ui
    
    * fix: everything
    
    * add init
    
    * add to readme
    
    * add to readme

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,8 @@
+import { supabase } from "@/auth/supabase";
+import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
+import { EyeIcon, EyeOffIcon } from "@/components/ui/icon";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { Text } from "@/components/ui/text";
 import {
   Toast,
@@ -6,7 +10,8 @@ import {
   ToastTitle,
   useToast,
 } from "@/components/ui/toast";
-import { useCallback } from "react";
+import { VStack } from "@/components/ui/vstack";
+import { useCallback, useState } from "react";
 import { ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
@@ -25,6 +30,7 @@ function useFetchHello() {
       toast.show({
         placement: "bottom",
         duration: 3000,
+        id: "hello-toast",
         render() {
           return (
             <Toast action="muted" variant="solid" className="min-w-40">
@@ -44,14 +50,130 @@ function useFetchHello() {
 }
 
 export default function HomeScreen() {
+  const toast = useToast();
+
   const insets = useSafeAreaInsets();
 
   const fetchHello = useFetchHello();
 
-  useLog("hello");
+  const [email, setEmail] = useState("");
 
-  // Example with object
-  useLog({ example: "hello" });
+  const [password, setPassword] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  async function signInWithEmail() {
+    setLoading(true);
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      toast.show({
+        placement: "bottom",
+        duration: 3000,
+        id: "login-error-toast",
+        render() {
+          return (
+            <Toast action="muted" variant="solid" className="min-w-40">
+              <ToastTitle>error</ToastTitle>
+
+              <ToastDescription>{error.message}</ToastDescription>
+            </Toast>
+          );
+        },
+      });
+    } else {
+      toast.show({
+        placement: "bottom",
+        duration: 3000,
+        id: "loggedin-toast",
+        render() {
+          return (
+            <Toast action="muted" variant="solid" className="min-w-40">
+              <ToastTitle>logged in</ToastTitle>
+
+              <ToastDescription>you loggedin</ToastDescription>
+            </Toast>
+          );
+        },
+      });
+    }
+
+    setLoading(false);
+  }
+
+  async function signUpWithEmail() {
+    setLoading(true);
+
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+
+      log(data);
+
+      if (error) {
+        toast.show({
+          placement: "bottom",
+          duration: 3000,
+          id: "signup-error-toast1",
+          render() {
+            return (
+              <Toast action="muted" variant="solid" className="min-w-40">
+                <ToastTitle>error</ToastTitle>
+
+                <ToastDescription>{error.message}</ToastDescription>
+              </Toast>
+            );
+          },
+        });
+      }
+
+      if (!data.session) {
+        log(data);
+
+        toast.show({
+          placement: "bottom",
+          duration: 3000,
+          id: "signup-error-toast2",
+          render() {
+            return (
+              <Toast action="muted" variant="solid" className="min-w-40">
+                <ToastTitle>No session</ToastTitle>
+
+                <ToastDescription>
+                  Please check your inbox for email verification!
+                </ToastDescription>
+              </Toast>
+            );
+          },
+        });
+      }
+    } catch (error: any) {
+      toast.show({
+        placement: "bottom",
+        duration: 3000,
+        id: "signup-error-toast3",
+        render() {
+          return (
+            <Toast action="muted" variant="solid" className="min-w-40">
+              <ToastTitle>error</ToastTitle>
+
+              <ToastDescription>{error.message}</ToastDescription>
+            </Toast>
+          );
+        },
+      });
+    }
+
+    setLoading(false);
+  }
 
   return (
     <ScrollView style={{ padding: insets.top }}>
@@ -60,6 +182,56 @@ export default function HomeScreen() {
       <Button onPress={() => fetchHello()}>
         <ButtonText>Fetch Hello</ButtonText>
       </Button>
+
+      <VStack space="xs" className="mt-8">
+        <Text className="text-typography-500">Email</Text>
+
+        <Input>
+          <InputField
+            autoComplete="email"
+            autoCapitalize={"none"}
+            autoCorrect={false}
+            value={email}
+            type="text"
+            onChangeText={setEmail}
+          />
+        </Input>
+      </VStack>
+
+      <VStack space="xs">
+        <Text className="text-typography-500">Password</Text>
+
+        <Input className="text-center">
+          <InputField
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChangeText={setPassword}
+            autoComplete="password"
+            autoCapitalize={"none"}
+            autoCorrect={false}
+            onSubmitEditing={() => signInWithEmail()}
+          />
+
+          <InputSlot
+            className="pr-3"
+            onPress={() => setShowPassword((cur) => !cur)}
+          >
+            <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+          </InputSlot>
+        </Input>
+      </VStack>
+
+      <Box className="py-4">
+        <Button disabled={loading} onPress={() => signInWithEmail()}>
+          <ButtonText>Sign in</ButtonText>
+        </Button>
+      </Box>
+
+      <Box className="py-4">
+        <Button disabled={loading} onPress={() => signUpWithEmail()}>
+          <ButtonText>Sign up</ButtonText>
+        </Button>
+      </Box>
     </ScrollView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import "react-native-url-polyfill/auto";
+import { supabase } from "@/auth/supabase";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 import { useColorScheme } from "@/hooks/useColorScheme";
@@ -6,11 +8,12 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { Session } from "@supabase/supabase-js";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
 import "../hooks/useLog"; // import useLog hook for global registration
 import "../util/log"; // relative path for reliable module resolution
@@ -19,6 +22,20 @@ import "../util/log"; // relative path for reliable module resolution
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useLog({ user: session?.user.email });
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   const colorScheme = useColorScheme();
 
   const [loaded] = useFonts({
