f0c7608a0f0e0370b643f3e5b74a15cd4b56d008
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-03-09 23:09:29 +0000

    fix

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -2,5 +2,6 @@ export { default as splash_icon } from "./splash-icon.png";
 export { default as react_logo } from "./react-logo.png";
 export { default as adaptive_icon } from "./adaptive-icon.png";
 export { default as favicon } from "./favicon.png";
+export { default as obi } from "./obi.png";
 export { default as partial_react_logo } from "./partial-react-logo.png";
 export { default as icon } from "./icon.png";
